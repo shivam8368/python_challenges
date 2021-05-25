85c3d93ad60fc97559780e579df0500aafacfba2
shivam kushwaha <shivamk8368@gmail.com>
2021-05-25 09:46:55 -0400

    String

--- prob_1.py
+++ prob_1.py
@@ -1,3 +1,14 @@
+# There is a sequence of words in CamelCase as a string of letters, , having the following properties:
+#
+# It is a concatenation of one or more words consisting of English letters.
+# All letters in the first word are lowercase.
+# For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+
+# Given str, determine the number of words in str .
+
+
+
+
 sample_string = "saveChangesInTheEditor"
 
 def camel_case(str):
